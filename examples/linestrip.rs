aeed11682f0fc608c69fc69edc0fd150456e69ce
Jonas Matser <github@jonasmatser.nl>
2021-06-19 21:36:33 +0200

    Start on quarry borehole example

--- examples/linestrip.rs
+++ examples/linestrip.rs
@@ -50,81 +50,33 @@ fn setup(
         });
 
     commands.spawn_bundle(PbrBundle {
-        mesh: asset_server.load("models/capped_cylinder2.glb#Mesh0/Primitive0"),
+        mesh: asset_server.load("models/pix4d_quarry_2.glb#Mesh0/Primitive0"),
         material: standard_materials.add(StandardMaterial {
-            base_color: Color::rgb(1.0, 1.0, 1.0),
+            base_color: Color::rgba(1.0, 1.0, 1.0, 0.5),
             ..Default::default()
         }),
         transform: Transform::from_xyz(-1.0, 0.0, 0.0),
         ..Default::default()
     });
 
-    // commands.spawn_bundle(PolylineBundle {
-    //     polyline: Polyline {
-    //         vertices: vec![Vec3::new(0.0, 0.0, 0.0), Vec3::new(5.0, 5.0, 0.0)],
-    //         ..Default::default()
-    //     },
-    //     render_pipelines: RenderPipelines {
-    //         pipelines: vec![new_polyline_pipeline(true)],
-    //         ..Default::default()
-    //     },
-    //     polyline_material: polyline_materials.add(PolylineMaterial {
-    //         width: 250.0,
-    //         perspective: false,
-    //         color: Color::RED,
-    //         ..Default::default()
-    //     }),
-    //     transform: Transform::from_xyz(0.0, 0.0, 0.0),
-    //     ..Default::default()
-    // });
-
-    // commands.spawn_bundle(PolylinePbrBundle {
-    //     polyline: Polyline {
-    //         vertices: vec![
-    //             Vec3::new(-0.5, 0.0, -0.5),
-    //             Vec3::new(0.5, 0.0, -0.5),
-    //             Vec3::new(0.5, 1.0, -0.5),
-    //             Vec3::new(-0.5, 1.0, -0.5),
-    //             Vec3::new(-0.5, 1.0, 0.5),
-    //             Vec3::new(0.5, 1.0, 0.5),
-    //             Vec3::new(0.5, 0.0, 0.5),
-    //             Vec3::new(-0.5, 0.0, 0.5),
-    //         ],
-    //         ..Default::default()
-    //     },
-    //     render_pipelines: RenderPipelines {
-    //         pipelines: vec![new_polyline_pbr_pipeline(true)],
-    //         ..Default::default()
-    //     },
-    //     polyline_pbr_material: polyline_pbr_materials.add(PolylinePbrMaterial {
-    //         width: 15.0,
-    //         perspective: false,
-    //         base_color: Color::WHITE,
-    //         ..Default::default()
-    //     }),
-    //     ..Default::default()
-    // });
+    // commands.spawn_scene(asset_server.load("models/pix4d_quarry_2.glb#Scene0"));
 
     // commands.spawn_bundle(PbrBundle {
-    //     mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-    //     material: standard_materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-    //     ..Default::default()
-    // });
-    // cube
-    // commands.spawn_bundle(PbrBundle {
-    //     mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-    //     material: standard_materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-    //     transform: Transform::from_xyz(0.0, 0.5, 0.0),
+    //     mesh: asset_server.load("models/capped_cylinder2.glb#Mesh0/Primitive0"),
+    //     material: standard_materials.add(StandardMaterial {
+    //         base_color: Color::rgb(1.0, 1.0, 1.0),
+    //         ..Default::default()
+    //     }),
+    //     transform: Transform::from_xyz(-1.0, 0.0, 0.0),
     //     ..Default::default()
     // });
 
     // light
     commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(0.0, 10.0, 20.0),
+        transform: Transform::from_xyz(0.0, 10.0, 0.0),
         point_light: PointLight {
-            // color: (),
-            // intensity: (),
-            range: 1000.0,
+            intensity: 40000.0,
+            range: 100000.0,
             // radius: (),
             ..Default::default()
         },
@@ -132,13 +84,12 @@ fn setup(
     });
 
     // camera
-    commands
-        .spawn_bundle(PerspectiveCameraBundle {
-            transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..PerspectiveCameraBundle::new_3d()
-        })
-        .insert(Rotates);
-    // });
+    let center = Vec3::new(0.0, -200.0, 0.0);
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(700.0, -200.0, 50.0).looking_at(center, Vec3::Y),
+        ..PerspectiveCameraBundle::new_3d()
+    });
+    // .insert(Rotates);
 }
 
 /// this component indicates what entities should rotate
