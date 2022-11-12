ba5e55a83773bff0542ccf84971a015837513061
Aevyrie Roessler <aevyrie@gmail.com>
2022-11-12 04:44:08 -0800

    hdr support and improved perspective blending

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -31,6 +31,10 @@ fn setup(
     // camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        camera: Camera {
+            hdr: true,
+            ..default()
+        },
         ..Camera3dBundle::default()
     });
 }
